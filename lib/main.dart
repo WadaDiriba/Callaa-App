397c43101b6229203f0499ca8c582dcca29e3145
WadaDiriba <wedadiriba@gmail.com>
2025-12-26 21:16:39 +0900

    new thing must be added here

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         title: 'Chella Rewards',
-        theme: AppTheme.lightTheme,
+        theme: AppTheme.darkTheme,
         debugShowCheckedModeBanner: false,
         initialRoute: '/login',
         routes: {
